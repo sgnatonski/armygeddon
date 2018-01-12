bd9c9a16fac56a542b134761a21011a282c1e039
sgnatonski <slawek.gnatonski@gmail.com>
2018-01-12 13:29:25 +0100

    added unit armor modifier

--- logic/damage_calculator.js
+++ logic/damage_calculator.js
@@ -1,12 +1,12 @@
 var calculator = {
     applyAttackDamage: (attacker, defender) => {
-        defender.endurance -= attacker.damage;
+        defender.endurance -= attacker.damage - defender.armor;
     },
     applyRangeDamage: (attacker, defender) => {
-        defender.endurance -= attacker.damage * (1 + Math.random());
+        defender.endurance -= attacker.damage * (1 + Math.random()) - defender.armor;
     },
     applyChargeDamage: (attacker, defender) =>{
-        defender.endurance -= attacker.damage + (attacker.charge * (1 + Math.random()));
+        defender.endurance -= attacker.damage + (attacker.charge * (1 + Math.random())) - defender.armor;
         attacker.charge = 0;
     }
 };
