059a18bc592f24c0938169fc460975541f79de6a
sgnatonski <slawek.gnatonski@gmail.com>
2018-01-23 15:58:12 +0100

    secured all apis with jwt token (naiive implementation with in-memory user databse)

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
 
 var index = require('./routes/index');
+var login = require('./routes/login');
 var battle = require('./routes/battle');
 var design = require('./routes/design');
 
@@ -13,6 +15,7 @@ process.chdir(__dirname);
 
 var app = express();
 
+app.set('TOKEN_SECRET', process.env.TOKEN_SECRET);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
@@ -26,7 +29,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(jwt({
+  secret: app.get('TOKEN_SECRET'),
+  getToken: function fromHeaderOrQuerystring (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
+}).unless({path: ['/login', '/register']}));
+
 app.use('/', index);
+app.use('/login', login);
 app.use('/battle', battle);
 if (app.get('env') === 'development'){
   app.use('/design', design);
@@ -41,13 +58,18 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  if (err.status == 401){
+    res.redirect('/login');
+  }
+  else{
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  }
 });
 
 module.exports = app;
---
+++ routes/login.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var crypto = require("crypto");
+var bcrypt = require("bcryptjs");
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+var users = [];
+
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Login'} );
+});
+
+router.post('/', function(req, res, next) {
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+  var user = users.find(u => u.pwdHash == hashedPassword);
+  if (!user){
+    var err = new Error('User not found');
+    err.status = 401;
+    next(err);
+    return;
+  }
+  
+  var token = jwt.sign({ id: user._id }, req.app.get('TOKEN_SECRET'), {
+    expiresIn: 86400 // expires in 24 hours
+  });
+  res.status(200).send({ auth: true, token: token });  
+});
+
+router.post('/register', function(req, res, next) {
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+  var user = {
+    id: crypto.randomBytes(8).toString("hex"),
+    name: req.body.name,
+    pwdHash: hashedPassword
+  };
+
+  users.push(user);
+  
+  var token = jwt.sign({ id: user.id }, req.app.get('TOKEN_SECRET'), {
+    expiresIn: 86400 // expires in 24 hours
+  });
+  res.status(200).send({ auth: true, token: token });  
+});
+
+module.exports = router;
