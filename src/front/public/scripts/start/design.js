977f4849cde5d33ba92acdb54c8e94863ffec7d0
Gnatonski <slawek.gnatonski@gmail.com>
2020-02-25 09:54:54 +0100

    removing prototype frontend implementation

--- src/front/public/scripts/start/design.js
+++
@@ -1,9 +0,0 @@
-(function start (){
-  var animator = new Animator();
-  var battle = new Game.DesignBattle();
-  
-  Promise.all([loadImages(), battle.load()]).then(r => {
-    var grid = initGrid(battle);
-    setupStage(grid, animator, r[0]);
-  })
-})();
